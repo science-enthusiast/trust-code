5107d3c5046e63eadc22a906bcf9ea939bf366fd
Pierre LEDAC <pierre.ledac@cea.fr>
2024-12-11 17:53:53 +0100

    [Kokkos] Port and optimization for ColdLegMixing benchmark

--- src/VEF/Operateurs/Op_Diff_Dift/Op_Diff_VEF_Face.cpp
+++ src/VEF/Operateurs/Op_Diff_Dift/Op_Diff_VEF_Face.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2024, CEA
+* Copyright (c) 2025, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -662,8 +662,8 @@ void Op_Diff_VEF_Face::ajouter_contribution(const DoubleTab& tab_transporte, Mat
   CDoubleTabView face_normale = domaine_VEF.face_normales().view_ro();
   CDoubleTabView nu = tab_nu.view_ro();
 
-  IntVect tab_face_associee(domaine_VEF.premiere_face_int());
-  IntVect tab_fac2b_idx(domaine_VEF.nb_faces());
+  IntTrav tab_face_associee(domaine_VEF.premiere_face_int());
+  IntTrav tab_fac2b_idx(domaine_VEF.nb_faces());
 
   Matrice_Morse_View matrice;
   matrice.set(tab_matrice);
@@ -699,8 +699,8 @@ void Op_Diff_VEF_Face::ajouter_contribution(const DoubleTab& tab_transporte, Mat
     }
 
   CIntArrView est_face_bord = domaine_VEF.est_face_bord().view_ro();
-  CIntArrView face_associee = tab_face_associee.view_ro();
-  CIntArrView fac2b_idx = tab_fac2b_idx.view_ro();
+  CIntArrView face_associee = static_cast<ArrOfInt&>(tab_face_associee).view_ro();
+  CIntArrView fac2b_idx = static_cast<ArrOfInt&>(tab_fac2b_idx).view_ro();
 
   auto ajouter_contrib = KOKKOS_LAMBDA(const int fac)
   {
