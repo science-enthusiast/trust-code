b4aa0e19b0b1fa95da2221b69823b81768deffb9
Hariprasad KANNAN <3788945+science-enthusiast@users.noreply.github.com>
2025-03-30 10:51:33 +0200

    Hierarchical parallelism within Op_Diff_VEF_Face::ajouter_cas_vectoriel in VEF/Operateurs/Op_Diff_Dift/Op_Diff_VEF_Face.cpp

--- src/VEF/Operateurs/Op_Diff_Dift/Op_Diff_VEF_Face.cpp
+++ src/VEF/Operateurs/Op_Diff_Dift/Op_Diff_VEF_Face.cpp
@@ -338,6 +338,12 @@ void Op_Diff_VEF_Face::ajouter_cas_vectoriel(const DoubleTab& inconnue,
   DoubleTabView resu_v = resu.view_rw();
   DoubleTabView tab_flux_bords_v = tab_flux_bords.view_rw();
 
+// #define BASIC_PARALLEL
+// #define MDRANGE_PARALLEL
+// #define SLOW_HP
+#define MDHP_NO_SCRATCH
+
+#if defined(BASIC_PARALLEL)
   auto kern_ajouter = KOKKOS_LAMBDA(int
                                     num_face, int k)
   {
@@ -365,6 +371,189 @@ void Op_Diff_VEF_Face::ajouter_cas_vectoriel(const DoubleTab& inconnue,
   };
   Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__), Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {nb_faces,2}) , kern_ajouter);
   end_gpu_timer(__KERNEL_NAME__);
+
+#elif defined(MDRANGE_PARALLEL)
+
+  auto kern_ajouter = KOKKOS_LAMBDA(int
+                                    num_face, int k)
+  {
+    int elem = face_voisins_v(num_face, k);
+    if (elem >= 0)
+      {
+        int ori = 1 - 2 * k;
+        double nu_elem = nu_v(elem, 0);
+        for (int i = 0; i < nb_comp; i++)
+          for (int j = 0; j < nb_comp; j++)
+            {
+              double grad_ij = grad_v(elem, i, j);
+              double grad_ji = grad_v(elem, j, i);
+              double fn = face_normales_v(num_face, j);
+              double flux = ori * fn * (nu_elem * grad_ij  /* + Re(elem, i, j) */ );
+              Kokkos::atomic_sub(&resu_v(num_face, i), flux);
+
+              if (num_face < nb_faces_bord)
+                {
+                  double flux_bord = ori * fn * (nu_elem * (grad_ij + grad_ji));
+                  Kokkos::atomic_sub(&tab_flux_bords_v(num_face, i), flux_bord);
+                }
+            }
+      }
+  };
+
+  Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__), Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0, 0}, {nb_faces, 2}), kern_ajouter);
+  end_gpu_timer(__KERNEL_NAME__);
+
+#elif defined(SLOW_HP)
+
+  typedef typename Kokkos::TeamPolicy<Kokkos::DefaultExecutionSpace>::member_type member_type;
+
+  parallel_for(start_gpu_timer(__KERNEL_NAME__), Kokkos::TeamPolicy<Kokkos::DefaultExecutionSpace>(nb_comp, Kokkos::AUTO),
+               KOKKOS_LAMBDA(const member_type &teamMember)
+  {
+    int i = teamMember.league_rank();
+
+    for (int k = 0; k < 2; k++)
+      {
+        Kokkos::parallel_for(Kokkos::TeamThreadRange(teamMember, nb_faces),
+                             [=] (int num_face)
+        {
+          int elem = face_voisins_v(num_face, k);
+          if (elem >= 0)
+            {
+              int ori = 1 - 2 * k;
+              double nu_elem = nu_v(elem, 0);
+              for (int j = 0; j < nb_comp; j++)
+                {
+                  double grad_ij = grad_v(elem, i, j);
+                  double grad_ji = grad_v(elem, j, i);
+                  double fn = face_normales_v(num_face, j);
+                  double flux = ori * fn * (nu_elem * grad_ij  /* + Re(elem, i, j) */ );
+                  Kokkos::atomic_sub(&resu_v(num_face, i), flux);
+
+                  if (num_face < nb_faces_bord)
+                    {
+                      double flux_bord = ori * fn * (nu_elem * (grad_ij + grad_ji));
+                      Kokkos::atomic_sub(&tab_flux_bords_v(num_face, i), flux_bord);
+                    }
+                }
+            }
+        });
+      }
+  });
+
+  end_gpu_timer(__KERNEL_NAME__);
+
+#elif defined(MDHP_NO_SCRATCH)
+
+  typedef typename Kokkos::TeamPolicy<Kokkos::DefaultExecutionSpace>::member_type member_type;
+
+  parallel_for(start_gpu_timer(__KERNEL_NAME__), Kokkos::TeamPolicy<Kokkos::DefaultExecutionSpace>(nb_faces, Kokkos::AUTO),
+               KOKKOS_LAMBDA(const member_type &teamMember)
+  {
+    int num_face = teamMember.league_rank();
+
+    Kokkos::parallel_for(Kokkos::TeamThreadMDRange<Kokkos::Rank<3>, member_type>(teamMember, 2, nb_comp, nb_comp),
+                         [=] (int k, int i, int j)
+    {
+      int elem = face_voisins_v(num_face, k);
+      if (elem >= 0)
+        {
+          int ori = 1 - 2 * k;
+          double nu_elem = nu_v(elem, 0);
+          double grad_ij = grad_v(elem, i, j);
+          double grad_ji = grad_v(elem, j, i);
+          double fn = face_normales_v(num_face, j);
+          double flux = ori * fn * (nu_elem * grad_ij  /* + Re(elem, i, j) */ );
+          Kokkos::atomic_sub(&resu_v(num_face, i), flux);
+
+          if (num_face < nb_faces_bord)
+            {
+              double flux_bord = ori * fn * (nu_elem * (grad_ij + grad_ji));
+              Kokkos::atomic_sub(&tab_flux_bords_v(num_face, i), flux_bord);
+            }
+        }
+    });
+  });
+
+  end_gpu_timer(__KERNEL_NAME__);
+
+#else
+
+  using ExecutionSpace = Kokkos::DefaultExecutionSpace;
+
+  Kokkos::TeamPolicy<ExecutionSpace> policy(nb_faces, Kokkos::AUTO);
+
+  typedef typename Kokkos::TeamPolicy<ExecutionSpace>::member_type member_type;
+
+  using ScratchPadView = Kokkos::View<double *, ExecutionSpace::scratch_memory_space>;
+
+  std::size_t scratch_num_elems = 27;
+
+  std::size_t scratch_num_bytes = ScratchPadView::shmem_size(scratch_num_elems*sizeof(double));
+
+  int level = 0;
+
+  Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__), policy.set_scratch_size(level, Kokkos::PerTeam(scratch_num_bytes)),
+                       KOKKOS_LAMBDA(const member_type &teamMember)
+  {
+    int num_face = teamMember.league_rank();
+
+    ScratchPadView scratch(teamMember.team_scratch(level), scratch_num_elems);
+
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(teamMember, 18), [=] (int s)
+    {
+      int k = s / 9;
+      int i = s / 3;
+      int j = s - (i * 3);
+      int elem = face_voisins_v(num_face, k);
+      scratch(s) = grad_v(elem, i, j);
+    });
+
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(teamMember, 9), [=] (int s)
+    {
+      if (s < 3)
+        scratch(s) = face_normales_v(num_face, s);
+      else if (s < 6)
+        scratch(s) = resu_v(num_face, s - 3);
+      else
+        scratch(s) = tab_flux_bords_v(num_face, s - 6);
+    });
+
+    Kokkos::parallel_for(Kokkos::TeamThreadMDRange<Kokkos::Rank<3>, member_type>(teamMember, 2, nb_comp, nb_comp),
+                         [=] (int k, int i, int j)
+    {
+      int elem = face_voisins_v(num_face, k);
+      if (elem >= 0)
+        {
+          int ori = 1 - 2 * k;
+          int s_1 = k * 9 + i * 3 + j;
+          int s_2 = k * 9 + j * 3 + i;
+          double nu_elem = nu_v(elem, 0);
+          double grad_ij = scratch(s_1);
+          double grad_ji = scratch(s_2);
+          double fn = scratch(18 + j);
+          double flux = ori * fn * (nu_elem * grad_ij  /* + Re(elem, i, j) */ );
+          Kokkos::atomic_sub(/* &resu_v(num_face, i) */ &scratch(21 + i), flux);
+
+          if (num_face < nb_faces_bord)
+            {
+              double flux_bord = ori * fn * (nu_elem * (grad_ij + grad_ji));
+              Kokkos::atomic_sub(/* &tab_flux_bords_v(num_face, i) */ &scratch(24 + i), flux_bord);
+            }
+        }
+    });
+
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(teamMember, 3), [=] (int s)
+    {
+      resu_v(num_face, s) = scratch(21 + s);
+      tab_flux_bords_v(num_face, s) = scratch(24 + s);
+    });
+
+  });
+
+  end_gpu_timer(__KERNEL_NAME__);
+
+#endif
 
   // Update flux_bords on symmetry:
   const int nb_bords=domaine_VEF.nb_front_Cl();
